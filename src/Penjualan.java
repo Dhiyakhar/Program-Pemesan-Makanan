3844a11218b9c24bc5decee3246699e4d6cd1ec7
Quinque <78577301+Dhiyakhar@users.noreply.github.com>
2021-07-07 21:29:15 +0700

    Add files via upload

---
+++ src/Penjualan.java
@@ -0,0 +1,10 @@
+
+package penjualan;
+
+public class Penjualan {
+
+    public static void main(String[] args) {
+        UIPenjualan penjualan = new UIPenjualan();	//User Interface Penjualan Makanan
+        penjualan.setVisible(true);					//Menampilkan layar
+    }
+}
